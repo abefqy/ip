5ff9314cee57e8a170ed7d5209c491920ee8dca8
abefqy <abefqy@gmail.com>
2025-08-27 12:41:42 +0800

    Rename chatbot and implemented skeletal behavior

---
+++ src/main/java/Darren.java
@@ -0,0 +1,13 @@
+public class Darren {
+    public static void main(String[] args) {
+
+        System.out.println("__________________________________");
+        System.out.println("Hello! I am Darren");
+        System.out.println("What can I do for you today?");
+        System.out.println("__________________________________");
+
+        System.out.println("Goodbye!");
+        System.out.println("__________________________________");
+
+    }
+}
