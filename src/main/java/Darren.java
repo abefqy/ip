05aa2b17d0fad1447aef91f4376bda563d3318d0
abefqy <abefqy@gmail.com>
2025-08-28 21:40:19 +0800

    Add echo feature and exit feature

--- src/main/java/Darren.java
+++ src/main/java/Darren.java
@@ -1,13 +1,27 @@
+import java.util.Scanner;
+
 public class Darren {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String logo = "Darren";
+        String line = "_____________________________________________";
 
-        System.out.println("__________________________________");
-        System.out.println("Hello! I am Darren");
-        System.out.println("What can I do for you today?");
-        System.out.println("__________________________________");
+        System.out.println(line);
+        System.out.println("Hello! I'm " + logo);
+        System.out.println("How can I help you?");
+        System.out.println(line);
 
-        System.out.println("Goodbye!");
-        System.out.println("__________________________________");
-
+        while (true) {
+            String input = scanner.nextLine();
+            if (input.equals("bye")) {
+                System.out.println(line);
+                System.out.println("Goodbye! Have a nice day :)");
+                System.out.println(line);
+                break;
+            }
+            System.out.println(line);
+            System.out.println(input);
+            System.out.println(line);
+        }
     }
 }
