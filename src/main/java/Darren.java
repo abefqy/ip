9c91f6d1ee6d03accc1ebb985571c54dbfe7d62d
abefqy <abefqy@gmail.com>
2025-08-28 23:21:55 +0800

    Add mark and unmark feature with Task class

--- src/main/java/Darren.java
+++ src/main/java/Darren.java
@@ -1,10 +1,18 @@
 import java.util.Scanner;
 
 public class Darren {
+
+    public static Integer getInt(String input) {
+        //get integer for mark and unmark
+        String[] words = input.split(" ");
+        return Integer.parseInt(words[1]);
+    }
+
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int taskCount = 0;
-        String[] tasks = new String[100];
+        Task[] tasks = new Task[100];
 
         String logo = "Darren";
         String line = "_____________________________________________";
@@ -23,15 +31,42 @@ public class Darren {
                 System.out.println("Goodbye! Have a nice day :)");
                 System.out.println(line);
                 break;
+
+
             } else if (input.equals("list")) {
                 //list feature
+                System.out.println("Here's your to-do list:");
                 System.out.println(line);
                 for (int i = 0; i < taskCount; i++) {
                     System.out.println( (i+1) + ". " + tasks[i]);
                 }
                 System.out.println(line);
+
+
+            } else if (input.startsWith("mark")) {
+                //mark feature
+                Integer index = getInt(input);
+                if (index != null && index <= taskCount && index > 0) {
+                    tasks[index - 1].markAsDone();
+                    System.out.println(line);
+                    System.out.println("Good work! This task has been complete:");
+                    System.out.println(tasks[index - 1]);
+                    System.out.println(line);
+                }
+
+            } else if (input.startsWith("unmark")) {
+                //unmark feature
+                Integer index = getInt(input);
+                if (index != null && index <= taskCount && index > 0) {
+                    tasks[index - 1].markAsNotDone();
+                    System.out.println(line);
+                    System.out.println("Got it, I've marked this task as incomplete");
+                    System.out.println(tasks[index - 1]);
+                    System.out.println(line);
+                }
             } else {
-                tasks[taskCount] = input;
+                // add task feature
+                tasks[taskCount] = new Task(input);
                 taskCount++;
                 System.out.println(line);
                 System.out.println("added: " + input);
